0d4be46043830797af87275b5916b85b295e6d56
lindeer <le.chang118@gmail.com>
2021-03-16 11:47:53 +0800

    set up test

--- xml_widget/lib/xml_widget.dart
+++ xml_widget/lib/xml_widget.dart
@@ -1,7 +1,91 @@
-library xml_widget;
+library durian;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+import 'package:flutter/widgets.dart';
+import 'package:xml/xml.dart';
+part 'basic.dart';
+part 'text.dart';
+
+abstract class XmlWidgetBuilder {
+  String get name;
+
+  bool get childless;
+
+  // children would be empty if childless is false
+  Widget build(XmlElement element, List<Widget> children);
+}
+
+abstract class CommonWidgetBuilder implements XmlWidgetBuilder {
+  final String _name;
+
+  const CommonWidgetBuilder(this._name);
+
+  String get name => _name;
+
+  bool get childless => false;
+
+}
+
+class WidgetAssembler {
+  final _builders = Map.of(_defaultBuilders);
+
+  WidgetAssembler({
+    List<XmlWidgetBuilder>? builders,
+  }) {
+    if (builders != null && builders.isNotEmpty) {
+      final map = {
+        for (final b in builders) b.name: b
+      };
+      _builders.addAll(map);
+    }
+  }
+
+  static const _noChild = const <Widget>[];
+  static const _builtinBuilders = <XmlWidgetBuilder>[
+    const _XmlTextBuilder(),
+  ];
+
+  static final _defaultBuilders = {
+    for (final builder in _builtinBuilders) builder.name: builder
+  };
+
+  Widget _assembleDefaultWidget(XmlElement element, List<Widget> children) {
+    return Row(
+      children: children,
+    );
+  }
+
+  Widget _assembleByElement(XmlElement element) {
+    final name = element.name.qualified;
+    final builder = _builders[name];
+
+    Widget w;
+    if (builder != null) {
+      final children = builder.childless ? _noChild
+          : _inflateChildren(element);
+      w = builder.build(element, children);
+    } else {
+      w = _assembleDefaultWidget(element, _inflateChildren(element));
+    }
+    return w;
+  }
+
+  List<Widget> _inflateChildren(XmlElement element) => element.children
+      .where((node) => node.nodeType == XmlNodeType.ELEMENT)
+      .map((e) => e as XmlElement)
+      .map((e) => _assembleByElement(e))
+      .toList(growable: false);
+
+  Widget fromFile(String path) {
+    final f = File(path);
+    return fromSource(f.readAsStringSync());
+  }
+
+  Widget fromSource(String source) {
+    final doc = XmlDocument.parse(source);
+    final root = doc.rootElement;
+    final assembler = WidgetAssembler();
+    return assembler._assembleByElement(root);
+  }
 }
--- xml_widget/test/xml_widget_test.dart
+++ xml_widget/test/xml_widget_test.dart
@@ -1,12 +1,35 @@
+import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:xml_widget/xml_widget.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('test text widget - plain text', () {
+    const xml = """
+<Text>
+widget from xml
+</Text>
+""";
+    final assembler = WidgetAssembler();
+    final widget = assembler.fromSource(xml);
+    expect(widget is Text, true);
+    final w = widget as Text;
+    expect(w.data, 'widget from xml');
+  });
+
+  test('test text widget - rich text', () {
+    const xml = '<Text>widget <span>from</span> xml</Text>';
+    final assembler = WidgetAssembler();
+    final widget = assembler.fromSource(xml);
+    expect(widget is RichText, true);
+    final w = widget as RichText;
+    expect(w.text.toPlainText(), 'widget from xml');
+    final span = w.text;
+    final children = <InlineSpan>[];
+    span.visitChildren((span) {
+      children.add(span);
+      return true;
+    });
+    expect(children.length, 3);
   });
 }
