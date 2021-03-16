c4529c3574df0575af444a1a1aa0de17bd9d10be
lindeer <le.chang118@gmail.com>
2021-03-16 15:36:11 +0800

    Text widget

--- xml_widget/lib/text.dart
+++ xml_widget/lib/text.dart
@@ -9,9 +9,55 @@ class _XmlTextBuilder extends CommonWidgetBuilder {
 
   @override
   Widget build(XmlElement element, List<Widget> children) {
-    final text = element.text;
-    return Text(
-      text,
-    );
+    final children = <XmlNode>[];
+    String text = '';
+    bool isPlain = true;
+    element.children.forEach((node) {
+      if (node.nodeType == XmlNodeType.TEXT) {
+        final txt = (node as XmlText).text.trim();
+        if (txt.isNotEmpty) {
+          children.add(node);
+          text += txt;
+        }
+      } else if (node.nodeType == XmlNodeType.ELEMENT) {
+        isPlain = false;
+        children.add(node);
+      }
+    });
+
+    if (isPlain) {
+      return Text(
+        text,
+      );
+    } else {
+      final span = _createSpans(children);
+      return RichText(
+        text: TextSpan(
+          children: span,
+        ),
+      );
+    }
+  }
+
+  List<InlineSpan> _createSpans(List<XmlNode> nodes) {
+    final span = <InlineSpan>[];
+    nodes.forEach((node) {
+      if (node.nodeType == XmlNodeType.TEXT) {
+        final txt = (node as XmlText).text.trim();
+        if (txt.isNotEmpty) {
+          span.add(TextSpan(
+            text: txt,
+          ));
+        }
+      } else if (node.nodeType == XmlNodeType.ELEMENT) {
+        final e = node as XmlElement;
+        final w = _assembleByElement(e);
+        span.add(WidgetSpan(
+          child: w,
+          alignment: PlaceholderAlignment.middle,
+        ));
+      }
+    });
+    return span;
   }
 }
