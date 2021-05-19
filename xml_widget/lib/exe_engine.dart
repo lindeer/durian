4c45748bfd115e95b214b76e40c6552f7e9a4cf3
lindeer <le.chang118@gmail.com>
2021-05-19 21:26:51 +0800

    attach engine future

--- xml_widget/lib/exe_engine.dart
+++ xml_widget/lib/exe_engine.dart
@@ -20,6 +20,19 @@ class _FakeEngine implements ScriptEngine {
   }
 }
 
+final _loading = Container(
+  constraints: const BoxConstraints.tightFor(
+    width: 64,
+    height: 64,
+  ),
+  alignment: Alignment.center,
+  child: const CircularProgressIndicator(
+    valueColor: AlwaysStoppedAnimation<Color>(
+      Colors.deepOrangeAccent,
+    ),
+  ),
+);
+
 class ExeEngineWidget extends InheritedWidget {
   static final _fake = ScriptEngine.fake();
   final ScriptEngine engine;
@@ -32,7 +45,24 @@ class ExeEngineWidget extends InheritedWidget {
   }
 
   static ScriptEngine of(BuildContext context) {
-    final ExeEngineWidget? widget = context.dependOnInheritedWidgetOfExactType<ExeEngineWidget>();
+    final widget = context.getElementForInheritedWidgetOfExactType<ExeEngineWidget>()?.widget as ExeEngineWidget?;
     return widget?.engine ?? _fake;
+  }
+
+  // only if engine is ready
+  static Widget attachAsync(Future<ScriptEngine> future, Widget child) {
+    return FutureBuilder<ScriptEngine>(
+      future: future,
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+          return ExeEngineWidget(
+            engine: snapshot.requireData,
+            child: child,
+          );
+        } else {
+          return _loading;
+        }
+      },
+    );
   }
 }
