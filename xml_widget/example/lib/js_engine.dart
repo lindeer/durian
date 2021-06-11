85b9568817c55182fe46f1f0f7b873e1fc98c1db
lindeer <le.chang118@gmail.com>
2021-06-11 20:04:59 +0800

    compat old demo

--- xml_widget/example/lib/js_engine.dart
+++ xml_widget/example/lib/js_engine.dart
@@ -4,13 +4,16 @@ import 'package:xml_widget/script_engine.dart';
 class JSEngine implements ScriptEngine {
   final js.JavascriptRuntime _rt;
   final String domain;
+  final String _data;
   final _exprHandler = <StatementType, String Function(String expr)>{};
 
-  JSEngine._(this._rt, this.domain);
+  JSEngine._(this._rt, this.domain, this._data);
 
   factory JSEngine({String prefix = 'page'}) {
     final runtime = js.getJavascriptRuntime(xhr: false);
-    final engine = JSEngine._(runtime, prefix);
+    final domain = prefix.isEmpty ? '' : '$prefix.';
+    final data = prefix.isEmpty ? '' : '$prefix.data.';
+    final engine = JSEngine._(runtime, domain, data);
     engine._exprHandler[StatementType.expression] = engine._prefix;
     engine._exprHandler[StatementType.condition] = engine._parseField;
     engine._exprHandler[StatementType.call] = engine._parseCall;
@@ -21,22 +24,22 @@ class JSEngine implements ScriptEngine {
 
   static final _reg = RegExp(r'[_a-zA-Z]\w*(\.\w+)*');
 
-  String _prefix(String expr) => expr.startsWith('item.') ? expr : "$domain.data.$expr";
+  String _prefix(String expr) => expr.startsWith('item.') ? expr : "$_data$expr";
 
   String _parseField(String expr) {
     return expr.replaceAllMapped(_reg, (m) {
       final name = m[0] ?? '';
-      return name == 'item' ? name : "$domain.data.$name";
+      return name == 'item' ? name : '$_data$name';
     });
   }
 
-  String _parseCall(String expr) => expr.startsWith('item.') ? '$expr();' : "$domain.$expr();";
+  String _parseCall(String expr) => expr.startsWith('item.') ? '$expr();' : "$domain$expr();";
 
   String _parseAssign(String expr) {
     int pos = expr.indexOf('=') + 1;
 
     if (pos > 0) {
-      final r = '$domain.data.${expr.substring(pos).trim()}';
+      final r = '$_data${expr.substring(pos).trim()}';
       final ret = "${expr.substring(0, pos)}$r";
       return ret;
     } else {
@@ -44,7 +47,7 @@ class JSEngine implements ScriptEngine {
     }
   }
 
-  String _prefix2(String expr) => "$domain.$expr";
+  String _prefix2(String expr) => "$domain$expr";
 
   @override
   String eval(String statement, {StatementType type = StatementType.expression}) {
