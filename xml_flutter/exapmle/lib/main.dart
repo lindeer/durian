df69e7fa0c1e89dacbec0358724058ccbc1cf64c
lindeer <le.chang118@gmail.com>
2021-06-25 15:48:47 +0800

    example app

--- xml_flutter/exapmle/lib/main.dart
+++ xml_flutter/exapmle/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter_html/flutter_html.dart';
 
 void main() {
   runApp(MyApp());
@@ -46,17 +48,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late Future<String> _html;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    _html = rootBundle.loadString('assets/juice-detail.wxml');
   }
 
   @override
@@ -73,41 +71,40 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: FutureBuilder<String>(
+        future: _html,
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return _loading;
+          } else if (snapshot.hasError) {
+            return SingleChildScrollView(
+              child: Container(
+                color: Colors.white,
+                child: Text('error: ${snapshot.error}\n${snapshot.stackTrace}'),
+              ),
+            );
+          } else {
+            final data = snapshot.requireData;
+            return _buildView(ctx, data);
+          }
+        },
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
+  Widget _buildView(BuildContext context, String html) {
+    return SingleChildScrollView(
+      child: Html(
+        data: html,
+      ),
+    );
+  }
 }
+
+const _loading = const Center(
+  child: CircularProgressIndicator(
+    valueColor: AlwaysStoppedAnimation<Color>(
+      Colors.deepOrangeAccent,
+    ),
+  ),
+);
